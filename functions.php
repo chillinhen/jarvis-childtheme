76e9164374d8cf4b5e206d06f1d54c05e4ea344e
chillinhen <c.hillen@gmx.net>
2015-09-10 07:49:19 +0200

    Final version

--- functions.php
+++ functions.php
@@ -5,34 +5,34 @@ add_action('after_setup_theme', 'jarvis_child_theme_setup');
 function jarvis_child_theme_setup() {
     
     //init scripts
-    if (!function_exists("my_scripts")) {
+    if (!function_exists("jarvis_childscripts")) {
         if (!is_admin()) {
 
-            function my_scripts() {
+            function jarvis_childscripts() {
 
 
-                wp_enqueue_script('features', get_stylesheet_directory_uri() . '/features.js', array('jquery' ), '1.0', true );
+                wp_enqueue_script('features', get_stylesheet_directory_uri() . '/js/features.js', array('jquery' ), '1.0', true );
             }
 
         }
     }
 
 
-    add_action('wp_enqueue_scripts', 'my_scripts');
+    add_action('wp_enqueue_scripts', 'jarvis_childscripts');
 
     //init styles
-    if (!function_exists("my_styles")) {
+    if (!function_exists("jarvis_childstyles")) {
 	if (!is_admin()) {
 
-	    function my_styles() {
+	    function jarvis_childstyles() {
                 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-                wp_enqueue_style('features', get_stylesheet_directory_uri() . '/features.css', 'style', '1.0', 'screen');
+                wp_enqueue_style('features', get_stylesheet_directory_uri() . '/css/features.css', 'style', '1.0', 'screen');
 				wp_enqueue_style('googlefont', 'https://fonts.googleapis.com/css?family=Oswald', 'style', '1.0', 'screen');
 	    }
 
 	}
     }
-    add_action('wp_enqueue_scripts', 'my_styles');
+    add_action('wp_enqueue_scripts', 'jarvis_childstyles');
     
 
    
